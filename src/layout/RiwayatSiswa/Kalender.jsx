60bcab16f8fc7fde58be4a651e4ad7466e0f2c19
diovalda <ferrenxx@gmail.com>
2024-08-19 16:09:57 +0700

    riwayat absen done

--- src/layout/RiwayatSiswa/Kalender.jsx
+++ src/layout/RiwayatSiswa/Kalender.jsx
@@ -1,20 +1,25 @@
-import React, { useContext } from 'react';
-import { Grid, useMediaQuery } from '@mui/material';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DateCalendar, DatePicker } from '@mui/x-date-pickers';
-import { TanggalContext } from './TanggalPIlih';
+import React, { useContext } from "react";
+import { Grid, useMediaQuery } from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar, DatePicker } from "@mui/x-date-pickers";
+import { TanggalContext } from "./TanggalPIlih";
 
 export default function KotakTiga() {
   const { setSelectedDate } = useContext(TanggalContext);
-  const isMobile = useMediaQuery((theme) => theme.breakpoints.down('sm'));
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
 
   return (
-    <Grid container justifyContent="center" alignItems="center" style={{ height: '40%' }}>
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      style={{ height: "40%" }}
+    >
       <Grid item>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           {isMobile ? (
@@ -22,16 +27,21 @@ export default function KotakTiga() {
               label="Pilih tanggal"
               slotProps={{
                 textField: {
-                  helperText: 'MM/DD/YYYY',
+                  helperText: "MM/DD/YYYY",
                 },
               }}
               onChange={handleDateChange}
             />
           ) : (
             <DateCalendar
-            sx={{ transform: 'scale(1 .3)', bgcolor: 'white', mt: 10, borderRadius: '6px' }}
-            onChange={handleDateChange}
-          />
+              sx={{
+                transform: "scale(1 .3)",
+                bgcolor: "white",
+                mt: 10,
+                borderRadius: "6px",
+              }}
+              onChange={handleDateChange}
+            />
           )}
         </LocalizationProvider>
       </Grid>
