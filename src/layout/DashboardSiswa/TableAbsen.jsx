516a5ba99627dbb7121c62f703989dcff85e95df
lullabybot <jeanputra61@gmail.com>
2024-10-17 10:25:38 +0700

    siswa responsive

--- src/layout/DashboardSiswa/TableAbsen.jsx
+++ src/layout/DashboardSiswa/TableAbsen.jsx
@@ -8,15 +8,21 @@ import {
   TableRow,
   Paper,
   Box,
+  Button,
+  Modal,
+  Typography
 } from "@mui/material";
 import client from "../../router/Client";
+import { formatDate } from "../../helpers/helper";
 
 export default function TableAbsen() {
   const [presensi, setPresensi] = useState([]);
-
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  
   useEffect(() => {
     client.get("presensi").then(({ data }) => {
-      // Tambahkan properti hari berdasarkan tanggal
       const updatedData = data.data.map((item) => {
         const dateObj = new Date(item.tanggal);
         const days = [
@@ -30,16 +36,14 @@ export default function TableAbsen() {
         ];
         const dayName = days[dateObj.getDay()];
 
-        // Format waktu_datang menjadi hanya "HH:mm"
         const waktuDatangFormatted = item.waktu_datang
           ? item.waktu_datang.substring(11, 16)
           : "tidak hadir";
 
-        // Format waktu_pulang menjadi hanya "HH:mm"
         const waktuPulangFormatted = item.waktu_pulang
           ? item.waktu_pulang.substring(11, 16)
           : item.waktu_datang
-          ? "belum pulang"  
+          ? "belum pulang"
           : "tidak hadir";
 
         return {
@@ -57,35 +61,30 @@ export default function TableAbsen() {
   return (
     <Box
       sx={{
-        width: "full",
+        width: {lg:"full", md:"100%", sm:"100%"},
         border: "1px",
         borderRadius: 2,
         p: 4,
         pt: 0,
         mb: 8,
         boxShadow: "2px 4px 8px rgba(0.5, 0.1, 0.1, 0.1)",
+        marginTop: "12vh",
+        
       }}
     >
+      <Typography variant="h4" sx={{display:"flex", justifyContent:"center"}}>Table</Typography>
       <TableContainer
         component={Paper}
-        sx={{ paddingTop: "20px", bgcolor: "transparent" }}
+        sx={{ paddingTop: "20px", bgcolor: "transparent", overflowX: { sm: "auto" },  width: '100%', }}
       >
-        <Table>
+        <Table sx={{ minWidth: 700 }} >
           <TableHead>
-            <TableRow>
+            <TableRow >
               <TableCell style={{ backgroundColor: "#ADD8E6" }}>Hari</TableCell>
-              <TableCell style={{ backgroundColor: "#ADD8E6" }}>
-                Tanggal
-              </TableCell>
-              <TableCell style={{ backgroundColor: "#ADD8E6" }}>
-                Absen
-              </TableCell>
-              <TableCell style={{ backgroundColor: "#ADD8E6" }}>
-                Pulang
-              </TableCell>
-              <TableCell style={{ backgroundColor: "#ADD8E6" }}>
-                Keterangan
-              </TableCell>
+              <TableCell style={{ backgroundColor: "#ADD8E6" }}>Tanggal</TableCell>
+              <TableCell style={{ backgroundColor: "#ADD8E6" }}>Absen</TableCell>
+              <TableCell style={{ backgroundColor: "#ADD8E6" }}>Pulang</TableCell>
+              <TableCell style={{ backgroundColor: "#ADD8E6" }}>Keterangan</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -101,6 +100,105 @@ export default function TableAbsen() {
           </TableBody>
         </Table>
       </TableContainer>
+      <Button onClick={handleOpen} sx={{
+          display: {lg:'none', md:'none', sm:'block'}}}>Buka Tabel</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 'auto',
+            width: "90vw",
+            bgcolor: 'background.paper',
+            boxShadow: 24,
+            p: 4,
+            overflowX: "scroll"
+          }}
+        >
+          <TableContainer
+            component={Paper}
+            sx={{
+              maxHeight: '400px', // Set a specific max height for the table body
+              bgcolor: 'transparent',
+              overflowY: 'auto', // Enable vertical scrolling
+            }}
+          >
+            <Table stickyHeader>
+              <TableBody>
+                {/* Baris pertama: Hari */}
+                <TableRow>
+                  <TableCell style={{ backgroundColor: "#ADD8E6", padding: "4px", fontSize: "0.875rem" }}>
+                    Hari
+                  </TableCell>
+                  {presensi.map((row, index) => (
+                    <TableCell key={index} style={{ padding: "4px", fontSize: "0.875rem" }}>
+                      {row.hari}
+                    </TableCell>
+                  ))}
+                </TableRow>
+
+                {/* Baris kedua: Tanggal */}
+                <TableRow>
+                  <TableCell style={{ backgroundColor: "#ADD8E6", padding: "4px", fontSize: "0.875rem" }}>
+                    Tanggal
+                  </TableCell>
+                  {presensi.map((row, index) => (
+                    <TableCell key={index} style={{ padding: "4px", fontSize: "0.875rem" }}>
+                      {formatDate(row.tanggal)}
+                    </TableCell>
+                  ))}
+                </TableRow>
+
+                {/* Baris ketiga: Absen */}
+                <TableRow>
+                  <TableCell style={{ backgroundColor: "#ADD8E6", padding: "4px", fontSize: "0.875rem" }}>
+                    Absen
+                  </TableCell>
+                  {presensi.map((row, index) => (
+                    <TableCell key={index} style={{ padding: "4px", fontSize: "0.875rem" }}>
+                      {row.waktu_datang}
+                    </TableCell>
+                  ))}
+                </TableRow>
+
+                {/* Baris keempat: Pulang */}
+                <TableRow>
+                  <TableCell style={{ backgroundColor: "#ADD8E6", padding: "4px", fontSize: "0.875rem" }}>
+                    Pulang
+                  </TableCell>
+                  {presensi.map((row, index) => (
+                    <TableCell key={index} style={{ padding: "4px", fontSize: "0.875rem" }}>
+                      {row.waktu_pulang}
+                    </TableCell>
+                  ))}
+                </TableRow>
+
+                {/* Baris kelima: Keterangan */}
+                <TableRow>
+                  <TableCell style={{ backgroundColor: "#ADD8E6", padding: "4px", fontSize: "0.875rem" }}>
+                    Keterangan
+                  </TableCell>
+                  {presensi.map((row, index) => (
+                    <TableCell key={index} style={{ padding: "4px", fontSize: "0.875rem" }}>
+                      {row.keterangan}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+
+        </Box>
+      </Modal>
     </Box>
   );
 }
